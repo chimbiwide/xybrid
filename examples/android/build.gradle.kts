a73e5fe433bdfd5cede5a57524bf9aed4f4de79b
Glenn Sonna <kenpachi.os@gmail.com>
2026-03-18 19:11:42 +0000

    Update build.gradle.kts

--- examples/android/build.gradle.kts
+++ examples/android/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.application") version "8.13.2" apply false
     id("com.android.library") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("com.vanniktech.maven.publish") version "0.30.0" apply false
 }
